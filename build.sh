aa0b19a8ab3e9ab7c44380562f95039b0213634d
turbojax07 <turbojax07@gmail.com>
2026-01-01 21:21:16 -0500

    Removing overlays folder and marking the pack with 26.2 support

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 zipname="$(basename "$(pwd)").zip"
 
 rm "$zipname"
-zip "$zipname" -r assets/ overlays/ pack.mcmeta pack.png LICENSE
+
+zip "$zipname" -r assets/ big_update/ fog_distance_change/ opengl_3.3/ pack.mcmeta pack.png LICENSE
